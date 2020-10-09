242b3dc0f852d95eee96858917cf9b96121b2911
Fredrik Norén <fredrik.jw.noren@gmail.com>
2020-10-09 12:32:59 +0200

    Bump derk

--- derk_fixed.py
+++ derk_fixed.py
@@ -1,7 +1,8 @@
-from gym_derk.envs import DerkEnv, ConnectionLostError
+from gym_derk import DerkSession, DerkAgentServer, DerkAppInstance
 from argparse import ArgumentParser
 import numpy as np
 import random
+import asyncio
 import logging
 logging.basicConfig(level = logging.INFO)
 
@@ -17,31 +18,28 @@ def agent_update(observations, actions, observation_keys, action_keys):
     actions[action_keys.CastingSlot.value] = 0
     actions[action_keys.ChangeFocus.value] = random.randint(5, 7)
 
-def main(env_args={ "mode": "server" }):
-  env = DerkEnv(**env_args)
-
+async def run_session(env: DerkSession):
   action_n = np.zeros((env.n_agents, len(env.action_keys)))
 
   while True:
-    observation_n = env.reset()
-    while True:
+    observation_n = await env.reset()
+    while not env.done:
       for i in range(env.n_agents):
         agent_update(observation_n[i, :], action_n[i, :], env.observation_keys, env.action_keys)
-      observation_n, reward_n, done_n, info = env.step(action_n)
-      if all(done_n):
-        print("Episode finished")
-        break
-  env.close()
+      observation_n, reward_n, done_n, info = await env.step(action_n)
 
 if __name__ == "__main__":
   parser = ArgumentParser()
   parser.add_argument("-s", "--server", action="store_true", dest="server", default=False)
   parser.add_argument("-c", "--connected", action="store_true", dest="connected", default=False)
   args = parser.parse_args()
-  while True:
-    try: main({ 'mode': "server" if args.server else ("connected" if args.connected else None) })
-    except ConnectionLostError as err:
-      print('Connection lost')
-      err.env.close()
-    if not args.server:
-      break
+  server = DerkAgentServer(run_session, port=8789 if args.server else 8788)
+  asyncio.get_event_loop().run_until_complete(server.start())
+  if args.server:
+    asyncio.get_event_loop().run_forever()
+  else:
+    app = DerkAppInstance(browser_logs=True)
+    asyncio.get_event_loop().run_until_complete(app.start())
+    asyncio.get_event_loop().run_until_complete(
+      app.run_session(agent_hosts='dual_local' if args.connected else 'single_local')
+    )
