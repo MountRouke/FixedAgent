a1cd2b2d7fa139dc08578795a2f473f1ffc273c6
Fredrik Norén <fredrik.jw.noren@gmail.com>
2020-10-18 13:05:51 +0200

    Only set log level in main

--- derk_fixed.py
+++ derk_fixed.py
@@ -4,7 +4,6 @@ import numpy as np
 import random
 import asyncio
 import logging
-logging.basicConfig(level = logging.INFO)
 
 def agent_update(observations, actions, observation_keys, action_keys):
   if observations[observation_keys.HasFocus.value] > 0 and observations[observation_keys.HeightFront5.value] > -0.5:
@@ -29,6 +28,7 @@ async def run_session(env: DerkSession):
       observation_n, reward_n, done_n, info = await env.step(action_n)
 
 if __name__ == "__main__":
+  logging.basicConfig(level = logging.INFO)
   parser = ArgumentParser()
   parser.add_argument("-s", "--server", action="store_true", dest="server", default=False)
   parser.add_argument("-c", "--connected", action="store_true", dest="connected", default=False)
